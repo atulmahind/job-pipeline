d7a1d73b923b6f1a4c60ed0d35b6e87970a1ed31
Atul Mahind <atulmahind@gmail.com>
2016-09-01 23:25:31 +0200

    - Remove start function from Application - Instead create a bean in the configuration class

--- src/main/java/in/mahind/bootquartz/spring/MySpringBeans.java
+++ src/main/java/in/mahind/bootquartz/spring/MySpringBeans.java
@@ -1,9 +1,21 @@
 package in.mahind.bootquartz.spring;
 
+import in.mahind.bootquartz.Application;
+import in.mahind.bootquartz.job.JobInit;
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
 import org.quartz.spi.JobFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
 
 /**
  * @author <a href="mailto:atul.mahind@gmail.com">Atul Mahind</a>
@@ -11,6 +23,44 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MySpringBeans {
+
+	@Value("#{new Integer('${pipeline.frequencyInSeconds}')}")
+	private int pipelineFrequency;
+
+	@Autowired
+	private JobFactory jobFactory;
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+	@Bean
+	public Scheduler pipelineStart() {
+		String jobName = "initJob";
+		String jobGroup = "initJobGroup";
+
+		JobDetail jobDetail = newJob(JobInit.class)
+				.withIdentity(jobName, jobGroup)
+				.usingJobData(new JobDataMap())
+				.build();
+
+		Trigger trigger = newTrigger()
+				.withIdentity(jobName + "Trigger", jobGroup + "Trigger")
+				.withSchedule(simpleSchedule()
+						.withIntervalInSeconds(pipelineFrequency)
+						.withMisfireHandlingInstructionNextWithRemainingCount()
+						.repeatForever())
+				.build();
+
+		Scheduler scheduler = null;
+		try {
+			scheduler = StdSchedulerFactory.getDefaultScheduler();
+			scheduler.setJobFactory(jobFactory);
+			scheduler.startDelayed(0);
+			scheduler.scheduleJob(jobDetail, trigger);
+		} catch (SchedulerException se) {
+			LOGGER.error("Unable to schedule Pipeline: {}", se.toString());
+		}
+		return scheduler;
+	}
 
 	@Bean
 	public JobFactory jobFactory(ApplicationContext applicationContext)
