c4c485f03c457480a7a9537f4ef55ebd4f53437e
Atul Mahind <atulmahind@gmail.com>
2016-09-01 11:46:01 +0200

    - Add autowiring support to quartz jobs - Add dependency for spring context support

---
+++ src/main/java/in/mahind/bootquartz/spring/AutowiringSpringBeanJobFactory.java
@@ -0,0 +1,34 @@
+package in.mahind.bootquartz.spring;
+
+import org.quartz.spi.TriggerFiredBundle;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.scheduling.quartz.SpringBeanJobFactory;
+
+/**
+ * Adds autowiring support to quartz jobs.
+ *
+ * <p>It is not Spring who instantiate the <tt>Job</tt> implementations, but Quartz, and Quartz does not know anything
+ * about dependency injection.</p>
+ *
+ * @see <a href="http://blog.btmatthews.com/?p=40">http://blog.btmatthews.com/?p=40</a>
+ */
+
+public final class AutowiringSpringBeanJobFactory extends SpringBeanJobFactory implements
+		ApplicationContextAware {
+
+    private transient AutowireCapableBeanFactory beanFactory;
+
+    @Override
+    public void setApplicationContext(final ApplicationContext context) {
+        beanFactory = context.getAutowireCapableBeanFactory();
+    }
+
+    @Override
+    protected Object createJobInstance(final TriggerFiredBundle bundle) throws Exception {
+        final Object job = super.createJobInstance(bundle);
+        beanFactory.autowireBean(job);
+        return job;
+    }
+}
